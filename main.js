c9c6f9eb6289961a0076d2d9ce66feb09bd7f267
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-04-15 11:26:26 +0100

    show Electron version

--- main.js
+++ main.js
@@ -5,56 +5,56 @@ const windows = require("./modules/windows");
 const alert = require("./modules/alert");
 
 electron.app.on("ready", () => {
-    windows.new({width: 1600, height: 300, page: "swarm.html"});
-    menu_build();
+	windows.new({width: 1600, height: 300, page: "swarm.html"});
+	menu_build();
 });
 
 electron.app.on("window-all-closed", () => {
-    electron.app.quit();
+	electron.app.quit();
 });
 
 function menu_build() {
-    const template = [
-        {
-            label: "Menu",
-            submenu: [
-                {
-                    label: "About",
-                    click: () => {
-                        alert.alert("This is a test program.");
-                    }
-                },
-                {
-                    type: "separator"
-                },
-                {
-                    label: "Zoom out",
-                    click: () => {
-                        windows.change_zoom(-0.1);
-                    }
-                },
-                {
-                    label: "Zoom in",
-                    click: () => {
-                        windows.change_zoom(0.1);
-                    }
-                },
-                {
-                    role: "reload"
-                },
-                {
-                    role: "quit"
-                },
-                {
-                    type: "separator"
-                },
-                {
-                    role: "toggledevtools"
-                }
-            ]
-        }
-    ];
+	const template = [
+		{
+			label: "Menu",
+			submenu: [
+				{
+					label: "About",
+					click: () => {
+						alert.alert("This is a test program running under Electron " + process.versions.electron);
+					}
+				},
+				{
+					type: "separator"
+				},
+				{
+					label: "Zoom out",
+					click: () => {
+						windows.change_zoom(-0.1);
+					}
+				},
+				{
+					label: "Zoom in",
+					click: () => {
+						windows.change_zoom(0.1);
+					}
+				},
+				{
+					role: "reload"
+				},
+				{
+					role: "quit"
+				},
+				{
+					type: "separator"
+				},
+				{
+					role: "toggledevtools"
+				}
+			]
+		}
+	];
 
-    const menu = electron.Menu.buildFromTemplate(template);
-    electron.Menu.setApplicationMenu(menu);
+	const menu = electron.Menu.buildFromTemplate(template);
+	electron.Menu.setApplicationMenu(menu);
 }
