b8ddebde5e5d3a452acfe0a0fd86d32eba4fe822
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2020-04-25 12:13:50 +0100

    Backport from Nibbler

--- modules/alert.js
+++ modules/alert.js
@@ -2,44 +2,20 @@
 
 const electron = require("electron");
 
-function object_to_string(o) {
-	let msg = JSON.stringify(o);
-	return msg;
-}
-
-function alert_main(msg) {
-	electron.dialog.showMessageBox({
-		message: msg.toString(),
-		title: "Alert",
-		buttons: ["OK"]
-	}, () => {});			// Providing a callback makes the window not block the process
-}
-
-function alert_renderer(msg) {
-	electron.remote.dialog.showMessageBox({
-		message: msg.toString(),
-		title: "Alert",
-		buttons: ["OK"]
-	}, () => {});
-}
-
 module.exports = (msg) => {
 	if (msg instanceof Error) {
 		msg = msg.toString();
 	}
-	if (typeof(msg) === "object") {
-		msg = object_to_string(msg);
+	if (typeof msg === "object") {
+		msg = JSON.stringify(msg);
 	}
-	if (typeof(msg) === "undefined") {
+	if (typeof msg === "undefined") {
 		msg = "undefined";
 	}
-	if (typeof(msg) === "number") {
-		msg = msg.toString();
-	}
-	msg = msg.trim()
-	if (process.type === "renderer") {
-		alert_renderer(msg);
-	} else {
-		alert_main(msg);
-	}
-}
+	msg = msg.toString().trim();
+	let fn = process.type === "renderer" ?
+		electron.remote.dialog.showMessageBox :
+		electron.dialog.showMessageBox;
+	fn({message: msg, title: "Alert", buttons: ["OK"]}, () => {});
+	// Providing a callback makes the window not block the process.
+};
